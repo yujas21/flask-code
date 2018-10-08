412406438d1811c0310e55396cff0bbfcf2e71ce
yujas21 <43471779+yujas21@users.noreply.github.com>
2018-10-08 16:44:33 +0530

    edited

--- code.py
+++ code.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/blog/<int:postID>')
 def show_blog(postID):
-   return 'Blog Number %d' % postID
+   return 'Blog No. %d' % postID
 
 @app.route('/rev/<float:revNo>')
 def revision(revNo):
-   return 'Revision Number %f' % revNo
+   return 'Revision No. %f' % revNo
 
 if __name__ == '__main__':
    app.run()
